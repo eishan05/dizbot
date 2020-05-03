8db01b082d22686ccfa9346a9523fc328a188014
Eishan Lawrence <eishanlawrence5@gmail.com>
2020-05-02 19:27:34 -0700

    Created a command group

--- dizbot/dizbot.py
+++ dizbot/dizbot.py
@@ -1,6 +1,12 @@
 import click
 
-@click.command()
-def cli():
-  click.echo("Hello Botters!")
+@click.group(invoke_without_command=True)
+@click.pass_context
+def cli(ctx):
+  if ctx.invoked_subcommand is None:
+    click.echo("You are using dizbot, a command line tool to help you seemlessly create discord bots in python!")
+
+@cli.command()
+def create():
+  click.echo("You discord bot will now be created")
 
