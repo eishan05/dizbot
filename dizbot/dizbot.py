f5c7718381dd7692e57dbc80bd861a485c3af48b
Eishan Lawrence <eishanlawrence5@gmail.com>
2020-05-03 15:28:39 -0700

    Added user intial user prompts

--- dizbot/dizbot.py
+++ dizbot/dizbot.py
@@ -8,5 +8,25 @@ def cli(ctx):
 
 @cli.command()
 def create():
-  click.echo("You discord bot will now be created")
+  if click.confirm("Do you want to add commands to the bot?"):
+    add_commands()
+  if click.confirm("Do you want to add event handlers to the bot?"):
+    add_event_handlers()
+  if click.confirm("Do you want dizbot to add your bot's client token to your bot for you?"):
+    add_client_token()
+  give_client_token_information()
+  click.secho("Bot created successfully!", fg="green")
 
+def add_commands():
+  click.echo("Adding commands!")
+
+def add_event_handlers():
+  click.echo("Adding event handlers!")
+
+def add_client_token():
+  click.echo("Adding client token")
+
+def give_client_token_information():
+  click.echo("You can add/change your bot's client token manually to the file client_token.txt")
+  click.secho("WARNING: Please make sure you keep your client token safe, and not give anyone else access to it!", bg="blue", fg="white", bold=True, blink=True)
+  click.secho("Please do not push the client_token.txt file to GitHub or any publicly accessible website", bg="blue", fg="white", bold=True, blink=True)
