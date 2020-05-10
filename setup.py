62ccf4e8001a2aa99ab226e62113e51aefe280d4
Eishan Lawrence <eishanlawrence5@gmail.com>
2020-05-09 23:24:29 -0700

    Fixed color issue on Windows command line

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 from setuptools import setup
 import dizbot
+import sys
+
+def get_requirements():
+  requirements = ["Click", "discord.py"]
+  if "win32" in sys.platform.lower():
+    requirements.append("colorama")
+  return requirements
 
 setup(
     name="dizbot",
@@ -10,8 +17,5 @@ setup(
     license=dizbot.__license__,
     packages=["dizbot"],
     entry_points={"console_scripts": ["dizbot=dizbot.dizbot:cli"]},
-    install_requires=[
-      "Click",
-      "discord.py"
-    ],
+    install_requires=get_requirements(),
 )
